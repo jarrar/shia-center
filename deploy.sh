b72f396148995f1f70f9ea8aea3f48abcb06c76e
Jarrar Jaffari <jjaffari@cisco.com>
2016-08-26 18:48:44 -0400

    fixed deploy.sh to take params

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (c) 1999 Cisco Systems, Inc.  All rights reserved.
 # AUTHOR:  Jarrar Jaffari (), jjaffari@cisco.com
-set -o nounset
+#set -o nounset
 readonly SCRIPT=${0##*/}
 
 function die() { echo $@; exit 1; }
@@ -12,33 +12,53 @@ trap cleanup EXIT
 function show_help()
 {
     cat <<-HELP
-    $SCRIPT [-h | -a ]
+    $SCRIPT [-d | -i | -h]
+
+    -d | --debug           prints details logs of ansible-playbook.
+    -i | --inventory  inventory file for an organization to deploy to.
+    -h | --help            shows this help.
 HELP
+}
+
+function init()
+{
+   os=$(uname)
+   if [[ $os == "Darwin" ]]
+   then
+        GETOPT=/usr/local/Cellar/gnu-getopt/1.1.6/bin/getopt
+   else
+        GETOPT=$(which getopt)
+   fi
 }
 
 #
 # main
 #
+DEBUG=""
+INVENTORY=""
 
-#TEMP=$(getopt -o a:h --long arga:,help -n $SCRIPT -- "$@")
-#eval set -- "$TEMP"
+init
+
+TEMP=$($GETOPT -o di:h --long debug,inventory:,help -n $SCRIPT -- "$@")
+eval set -- "$TEMP"
 
 # extract options and their arguments into variables.
-#while true ; do
-#    case "$1" in
-#        -a|--arga)
-#                ARG_A=$2 ; shift 2 ;;
-#        -h|--help)
-#             show_help
-#             exit 0
-#             ;;
-#        --) shift ; break ;;
-#        *) echo "Internal error!" ; exit 1 ;;
-#    esac
-#done
+while true ; do
+    case "$1" in
+        -d|--debug)
+                DEBUG="-vvvv" ; shift 1 ;;
+        -i|--inventory)
+                INVENTORY=$2 ; shift 2 ;;
+        -h|--help)
+             show_help
+             exit 0
+             ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
 
-# defaults
-#ARG_A=${ARG_A:='Some value'}
+#[[ -z $INVENTORY ]] && die "Missing inventory file"
 
 COPYFILE_DISABLE=1 tar -czvf web.tar.gz web
-time ansible-playbook -vvvv -i deploy/iabat.inv deploy/deploy-web.yml
+time ansible-playbook $DEBUG -i $INVENTORY deploy/deploy-web.yml
